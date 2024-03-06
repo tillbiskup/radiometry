1d8913f0fd23401cc04da1dce32e6e76e4d4255f
Till Biskup <till.biskup@ptb.de>
2024-03-06 14:28:10 +0100

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -120,12 +120,12 @@ html_theme = "sphinx_rtd_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = ""
+html_logo = "images/radiometry-logo.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = ""
+html_favicon = "images/radiometry-logo-favicon.ico"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
